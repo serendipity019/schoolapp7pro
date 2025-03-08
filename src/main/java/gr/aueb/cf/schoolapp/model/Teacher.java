81975d160c380a079681bd77f5d5051a0dec2316
serendipity019 <navagio09@gmail.com>
2025-03-08 11:47:32 +0200

    feat: Teacher model created

---
+++ src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
@@ -0,0 +1,155 @@
+package gr.aueb.cf.schoolapp.model;
+
+import java.time.LocalDateTime;
+
+public class Teacher {
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String vat;
+    private String fatherName;
+    private String phoneNum;
+    private String email;
+    private String street;
+    private String streetNum;
+    private String zipCode;
+    private Integer cityId;
+    private String uuid;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    //private Boolean enabled; // soft delete
+
+    public Teacher() {}
+
+    public Teacher(Integer id, String firstname, String lastname, String vat,
+                   String fatherName, String phoneNum, String email, String street,
+                   String streetNum, String zipCode, Integer cityId, String uuid,
+                   LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.vat = vat;
+        this.fatherName = fatherName;
+        this.phoneNum = phoneNum;
+        this.email = email;
+        this.street = street;
+        this.streetNum = streetNum;
+        this.zipCode = zipCode;
+        this.cityId = cityId;
+        this.uuid = uuid;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getVat() {
+        return vat;
+    }
+
+    public void setVat(String vat) {
+        this.vat = vat;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNum() {
+        return streetNum;
+    }
+
+    public void setStreetNum(String streetNum) {
+        this.streetNum = streetNum;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
