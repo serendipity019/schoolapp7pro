ea30cae535edb9b165f9f1509a7e859c9d460e40
serendipity019 <navagio09@gmail.com>
2025-03-11 19:24:54 +0200

    feat: Exceptions for teacher not found and teacher already exists  created

---
+++ src/main/java/gr/aueb/cf/schoolapp/exceptions/TeacherAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.schoolapp.exceptions;
+
+public class TeacherAlreadyExistsException extends Exception {
+
+    public TeacherAlreadyExistsException(String message) {
+        super(message);
+    }
+}
